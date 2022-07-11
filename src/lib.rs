4e7216e55c7b50191f159e024b72274d9ea53f23
Maximous Black <maximousblk@gmail.com>
2022-07-11 12:47:15 +0000

    chore: update code formatting style

--- src/lib.rs
+++ src/lib.rs
@@ -5,95 +5,90 @@ use sha2::{Digest, Sha256};
 use std::ops::Rem;
 
 pub struct Wallet {
-    pub pvtkey: RsaPrivateKey,
-    pub pubkey: RsaPublicKey,
+  pub pvtkey: RsaPrivateKey,
+  pub pubkey: RsaPublicKey,
 }
 
 impl Wallet {
-    pub fn new() -> Self {
-        let mut rng = rand::thread_rng();
+  pub fn new() -> Self {
+    let mut rng = rand::thread_rng();
 
-        let pvtkey = RsaPrivateKey::new_with_exp(
-            &mut rng,
-            4096,
-            &BigUint::from_bytes_be(&[0x01, 0x00, 0x01]),
-        )
-        .unwrap();
-        let pubkey = RsaPublicKey::from(&pvtkey);
+    let pvtkey = RsaPrivateKey::new_with_exp(&mut rng, 4096, &BigUint::from_bytes_be(&[0x01, 0x00, 0x01])).unwrap();
+    let pubkey = RsaPublicKey::from(&pvtkey);
 
-        Wallet { pvtkey, pubkey }
+    Wallet { pvtkey, pubkey }
+  }
+
+  pub fn address(&self) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(self.components().n.to_bytes_be());
+    encode(&hasher.finalize()[..])
+  }
+
+  pub fn components(&self) -> Components {
+    let d = self.pvtkey.d();
+    let p = &self.pvtkey.primes()[0];
+    let q = &self.pvtkey.primes()[1];
+
+    Components {
+      e: self.pubkey.e().clone(),
+      d: d.clone(),
+
+      p: p.clone(),
+      q: q.clone(),
+      n: self.pubkey.n().clone(),
+
+      dp: d.rem(p - BigUint::from(1u8)),
+      dq: d.rem(q - BigUint::from(1u8)),
+      qi: p.modpow(&(q - BigUint::from(2u8)), &q),
     }
+  }
 
-    pub fn address(&self) -> String {
-        let mut hasher = Sha256::new();
-        hasher.update(self.components().n.to_bytes_be());
-        encode(&hasher.finalize()[..])
+  pub fn jwk(&self) -> JWK {
+    JWK {
+      kty: "RSA".to_string(),
+      ext: true,
+
+      d: encode(&self.components().d.to_bytes_be()),
+      e: encode(&self.components().e.to_bytes_be()),
+
+      p: encode(&self.components().p.to_bytes_be()),
+      q: encode(&self.components().q.to_bytes_be()),
+      n: encode(&self.components().n.to_bytes_be()),
+
+      dp: encode(&self.components().dp.to_bytes_be()),
+      dq: encode(&self.components().dq.to_bytes_be()),
+      qi: encode(&self.components().qi.to_bytes_be()),
     }
-
-    pub fn components(&self) -> Components {
-        let d = self.pvtkey.d();
-        let p = &self.pvtkey.primes()[0];
-        let q = &self.pvtkey.primes()[1];
-
-        Components {
-            e: self.pubkey.e().clone(),
-            d: d.clone(),
-
-            p: p.clone(),
-            q: q.clone(),
-            n: self.pubkey.n().clone(),
-
-            dp: d.rem(p - BigUint::from(1u8)),
-            dq: d.rem(q - BigUint::from(1u8)),
-            qi: p.modpow(&(q - BigUint::from(2u8)), &q),
-        }
-    }
-
-    pub fn jwk(&self) -> JWK {
-        JWK {
-            kty: "RSA".to_string(),
-            ext: true,
-
-            d: encode(&self.components().d.to_bytes_be()),
-            e: encode(&self.components().e.to_bytes_be()),
-
-            p: encode(&self.components().p.to_bytes_be()),
-            q: encode(&self.components().q.to_bytes_be()),
-            n: encode(&self.components().n.to_bytes_be()),
-
-            dp: encode(&self.components().dp.to_bytes_be()),
-            dq: encode(&self.components().dq.to_bytes_be()),
-            qi: encode(&self.components().qi.to_bytes_be()),
-        }
-    }
+  }
 }
 
 pub struct Components {
-    e: BigUint,
-    d: BigUint,
+  e: BigUint,
+  d: BigUint,
 
-    p: BigUint,
-    q: BigUint,
-    n: BigUint,
+  p: BigUint,
+  q: BigUint,
+  n: BigUint,
 
-    dp: BigUint,
-    dq: BigUint,
-    qi: BigUint,
+  dp: BigUint,
+  dq: BigUint,
+  qi: BigUint,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct JWK {
-    kty: String,
-    ext: bool,
+  kty: String,
+  ext: bool,
 
-    e: String,
-    d: String,
+  e: String,
+  d: String,
 
-    p: String,
-    q: String,
-    n: String,
+  p: String,
+  q: String,
+  n: String,
 
-    dp: String,
-    dq: String,
-    qi: String,
+  dp: String,
+  dq: String,
+  qi: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -12,89 +12,79 @@ use wave::Wallet;
 #[derive(Parser)]
 #[clap(author, version = {option_env!("RELEASE_VERSION").unwrap_or("dev")}, about, arg_required_else_help(true))]
 struct Args {
-    /// The number of threads to use
-    #[clap(short, long, value_parser, default_value_t = 8)]
-    threads: u8,
+  /// The number of threads to use
+  #[clap(short, long, value_parser, default_value_t = 8)]
+  threads: u8,
 
-    /// The path to the output directory
-    #[clap(short, long, value_parser, default_value("./wallets"))]
-    output: String,
+  /// The path to the output directory
+  #[clap(short, long, value_parser, default_value("./wallets"))]
+  output: String,
 
-    /// Pattern to use for the wallet names
-    #[clap(value_parser)]
-    pattern: String,
+  /// Pattern to use for the wallet names
+  #[clap(value_parser)]
+  pattern: String,
 }
 
 fn main() {
-    let current_version = option_env!("RELEASE_VERSION").unwrap_or("dev");
+  let current_version = option_env!("RELEASE_VERSION").unwrap_or("dev");
 
-    let informer = update_informer::new(registry::GitHub, "maximousblk/wave.rs", current_version);
+  let informer = update_informer::new(registry::GitHub, "maximousblk/wave.rs", current_version);
 
-    if let Some(version) = informer.check_version().ok().flatten() {
-        let msg = format!(
-            "A new release of {} is available: v{} -> {}",
-            "wave".italic().cyan(),
-            current_version,
-            version.to_string().green()
-        );
+  if let Some(version) = informer.check_version().ok().flatten() {
+    let msg = format!("A new release of {} is available: v{} -> {}", "wave".italic().cyan(), current_version, version.to_string().green());
 
-        let release_url = format!(
-            "https://github.com/maximousblk/wave.rs/releases/tag/{}",
-            version
-        )
-        .yellow();
+    let release_url = format!("https://github.com/maximousblk/wave.rs/releases/tag/{}", version).yellow();
 
-        println!("\n{msg}\n{url}", msg = msg, url = release_url);
+    println!("\n{msg}\n{url}", msg = msg, url = release_url);
+  }
+
+  let args = Args::parse();
+
+  let pattern = Regex::new(&args.pattern).expect("Invalid pattern");
+
+  let outdir = PathBuf::from(&args.output);
+
+  if !outdir.exists() || !outdir.is_dir() {
+    fs::create_dir_all(&outdir).expect("Failed to create output directory");
+  }
+
+  println!("\n🔍 Pattern: /{}/", &args.pattern);
+  println!("📁 Output directory: {}", &outdir.display());
+  println!("🧵 Threads: {}\n", &args.threads);
+
+  let (tx, rx) = mpsc::channel();
+
+  let now = Instant::now();
+
+  for t in 0..args.threads {
+    let tx = tx.clone();
+    thread::spawn(move || loop {
+      tx.send((t, Wallet::new())).expect("Failed to send jwt");
+    });
+  }
+
+  let mut count: u32 = 0;
+  for (t, wallet) in rx {
+    count += 1;
+    let address = &wallet.address();
+    if pattern.is_match(address) {
+      println!("\n✅ [T{:02}] wallet: {}", t, address);
+
+      // write wallet jwk to file
+      let outfile = outdir.join(format!("arweave-keyfile-{}.json", address));
+
+      let jwk_json = serde_json::to_string(&wallet.jwk()).expect("Failed to serialize jwk");
+
+      fs::write(&outfile, jwk_json).expect("Failed to write wallet to file");
+
+      println!("📄 wallet written to file {}\n", outfile.display());
+
+      let el = now.elapsed().as_secs_f32();
+      println!("🏁 Done in {:.3}s ({:.1}/s)", el, (count as f32 / el));
+
+      break;
+    } else {
+      println!("🔍 [T{:02}] wallet: {}", t, address);
     }
-
-    let args = Args::parse();
-
-    let pattern = Regex::new(&args.pattern).expect("Invalid pattern");
-
-    let outdir = PathBuf::from(&args.output);
-
-    if !outdir.exists() || !outdir.is_dir() {
-        fs::create_dir_all(&outdir).expect("Failed to create output directory");
-    }
-    let outdir = outdir.canonicalize().expect("Failed to get canonical path");
-
-    println!("\n🔍 Pattern: /{}/", &args.pattern);
-    println!("📁 Output directory: {}", &outdir.display());
-    println!("🧵 Threads: {}\n", &args.threads);
-
-    let (tx, rx) = mpsc::channel();
-
-    let now = Instant::now();
-
-    for t in 0..args.threads {
-        let tx = tx.clone();
-        thread::spawn(move || loop {
-            tx.send((t, Wallet::new())).expect("Failed to send jwt");
-        });
-    }
-
-    let mut count: u32 = 0;
-    for (t, wallet) in rx {
-        count += 1;
-        let address = &wallet.address();
-        if pattern.is_match(address) {
-            println!("\n✅ [T{:02}] wallet: {}", t, address);
-
-            // write wallet jwk to file
-            let outfile = outdir.join(format!("arweave-keyfile-{}.json", address));
-
-            let jwk_json = serde_json::to_string(&wallet.jwk()).expect("Failed to serialize jwk");
-
-            fs::write(&outfile, jwk_json).expect("Failed to write wallet to file");
-
-            println!("📄 wallet written to file {}\n", outfile.display());
-
-            let el = now.elapsed().as_secs_f32();
-            println!("🏁 Done in {:.3}s ({:.1}/s)", el, (count as f32 / el));
-
-            break;
-        } else {
-            println!("🔍 [T{:02}] wallet: {}", t, address);
-        }
-    }
+  }
 }
